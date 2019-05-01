da86c5fd6f0428594795a6a10d1cc98675b6f034
Richard Turner <rclt201@gmail.com>
2019-05-01 22:15:06 +0100

    added parrallax and scolling animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+$(document).ready(function () {
+
+    $('#element-to-animate1').css('opacity', 0);
+    $('#element-to-animate1').waypoint(function () {
+        $('#element-to-animate1').addClass('fadeInUp');
+    }, { offset: '70%' });
+
+    $('#element-to-animate2').css('opacity', 0);
+    $('#element-to-animate2').waypoint(function () {
+        $('#element-to-animate2').addClass('fadeInUp');
+    }, { offset: '70%' });
+
+})
