753d2e239daa531f9c08003f39a79c5dec437147
Richard Turner <rclt201@gmail.com>
2019-05-02 11:56:19 +0100

    looking good. need to add projects

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
 
+    $('#element-to-animate0').css('opacity', 0);
+    $('#element-to-animate0').waypoint(function () {
+        $('#element-to-animate0').addClass('fadeInUp');
+    }, { offset: '70%' });
+
     $('#element-to-animate1').css('opacity', 0);
     $('#element-to-animate1').waypoint(function () {
         $('#element-to-animate1').addClass('fadeInUp');
-    }, { offset: '70%' });
+    }, { offset: '90%' });
 
     $('#element-to-animate2').css('opacity', 0);
     $('#element-to-animate2').waypoint(function () {
         $('#element-to-animate2').addClass('fadeInUp');
-    }, { offset: '70%' });
+    }, { offset: '90%' });
 
 })
