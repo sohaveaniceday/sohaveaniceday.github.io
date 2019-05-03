d9497b2419e79a7c2f0e3c5a1792b7a8ad9537e4
Richard Turner <rclt201@gmail.com>
2019-05-03 10:27:10 +0100

    fixing parallax fior mobile

--- scripts/main.js
+++ scripts/main.js
@@ -41,6 +41,35 @@ $(document).ready(function () {
         alterClassDesktop();
     });
 
+
+    /* resize the image(s) on page resize */
+    $(window).on("resize", function() {
+    resize_all_parallax();
+    });
+
+    /* keep all of your resize function calls in one place so you don't have to edit them twice (on page load and resize) */
+    function resize_all_parallax() {
+    var div_class = "parallax1"; /* the ID of the div that you're resizing */
+    var img_w = 1000; /* the width of your image, in pixels */
+    var img_h = 1000; /* the height of your image, in pixels */
+    resize_parallax(div_class, img_w, img_h);
+    }
+
+    /* this resizes the parallax image down to an appropriate size for the viewport */
+    function resize_parallax(div_class, img_w, img_h) {
+    var div = $("." + div_class);
+    var divwidth = div.width();
+    if (divwidth < 769) {
+        var pct = (img_h / img_w) * 105;
+    } /* show full image, plus a little padding, if on static mobile view */ else {
+        var pct = 60;
+    } /* this is the HEIGHT as percentage of the current div WIDTH. you can change it to show more (or less) of your image */
+    var newheight = Math.round(divwidth * (pct / 100));
+    newheight = newheight + "px";
+    div.height(newheight);
+    }
+
+    resize_all_parallax();
     alterClassTablet();
     alterClassDesktop();
 
